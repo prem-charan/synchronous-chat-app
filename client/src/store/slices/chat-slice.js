b008093173f6aa7a900d71403dd9293f25f32e32
PremCharan <premxcharan@gmail.com>
2025-04-20 12:29:14 +0530

    getting user contacts in the chat page

--- client/src/store/slices/chat-slice.js
+++ client/src/store/slices/chat-slice.js
@@ -2,27 +2,35 @@ export const createChatSlice = (set, get) => ({
   selectedChatType: undefined,
   selectedChatData: undefined,
   selectedChatMessages: [],
+  directMessagesContacts: [],
   setSelectedChatType: (selectedChatType) => set({ selectedChatType }),
   setSelectedChatData: (selectedChatData) => set({ selectedChatData }),
-  setselectedChatMessages: (setselectedChatMessages) => set({setselectedChatMessages}), 
+  setSelectedChatMessages: (messages) =>
+    set({ selectedChatMessages: messages }),
+  setDirectMessagesContacts: (directMessagesContacts) => set({ directMessagesContacts }),
   closeChat: () =>
-    set({ selectedChatData: undefined, selectedChatType: undefined, selectedChatMessages: [], }),
+    set({
+      selectedChatData: undefined,
+      selectedChatType: undefined,
+      selectedChatMessages: [],
+    }),
   addMessage: (message) => {
     const selectedChatMessages = get().selectedChatMessages;
     const selectedChatType = get().selectedChatType;
 
     set({
       selectedChatMessages: [
-        ...selectedChatMessages, {
+        ...selectedChatMessages,
+        {
           ...message,
           recipient:
-          selectedChatType === "channel"
-          ? message.recipient
-          : message.recipient._id,
+            selectedChatType === "channel"
+              ? message.recipient
+              : message.recipient._id,
           sender:
-          selectedChatType === "channel"
-          ? message.sender
-          : message.sender._id,
+            selectedChatType === "channel"
+              ? message.sender
+              : message.sender._id,
         },
       ],
     });
